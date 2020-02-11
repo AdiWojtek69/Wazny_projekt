ae860fa60b9dc1e050441e05c8a403151cf67b8e
Adrian <adigodopoczta@gmail.com>
2020-02-11 20:17:34 +0100

    drugi commit

--- Projekcik/src/com/company/Main.java
+++ Projekcik/src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	int a = 10;
         System.out.println(a);
+
+        int dwojka =2;
+        System.out.println(dwojka);
+
     }
 }
