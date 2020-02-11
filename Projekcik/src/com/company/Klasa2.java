0cf6f9c9117e18c1f80b2240cbcb61251daf0cd1
Adrian <adigodopoczta@gmail.com>
2020-02-11 20:33:46 +0100

    czwarty commit

--- Projekcik/src/com/company/Klasa2.java
+++ Projekcik/src/com/company/Klasa2.java
@@ -2,11 +2,13 @@ package com.company;
 
 public class Klasa2 {
 
-   
+
     int d=4;
 
-    void funkcja (){
-        System.out.println("KOCHAM CIE");
-    }
+int b =10;
+void funkcja5(){
+    System.out.println("ELOSZKA");
+}
+
 
 }
